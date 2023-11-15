6ada565563f969de875941b1df97b176cc7213cd
souta <e1b21116@oit.ac.jp>
2023-11-15 22:18:19 +0900

    ファイル読み込み機能の追加

--- scanner.c
+++ scanner.c
@@ -1,5 +1,28 @@
 #include<stdio.h>
-int main(){
-	printf("abc\n");
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc,char *argv[]){
+	
+	FILE *fp = NULL;
+	int c;
+
+	if(argc != 2){
+		fprintf(stderr,"ファイルを指定してください\n");
+		exit(1);
+	}
+
+	if((fp = fopen(argv[1],"r")) == NULL){
+        fprintf(stderr,"ファイルがありません\n");
+        exit(1);
+    }
+
+	while ((c=fgetc(fp))!=EOF){
+		printf("%c",c);
+
+	}
+	
+	printf("\n");
+	
 	return 0;
 }
